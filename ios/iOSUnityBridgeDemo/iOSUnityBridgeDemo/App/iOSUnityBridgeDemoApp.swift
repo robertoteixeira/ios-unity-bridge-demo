320249cf5d93e5d7297087349f536c7892a42ee3
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2026-06-09 18:41:04 -0300

    Organize native iOS project structure

--- ios/iOSUnityBridgeDemo/iOSUnityBridgeDemo/App/iOSUnityBridgeDemoApp.swift
+++ ios/iOSUnityBridgeDemo/iOSUnityBridgeDemo/App/iOSUnityBridgeDemoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct iOSUnityBridgeDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            UnityControlView()
         }
     }
 }
--- ios/iOSUnityBridgeDemo/iOSUnityBridgeDemo/UnityControlView.swift
+++ ios/iOSUnityBridgeDemo/iOSUnityBridgeDemo/UnityControlView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  UnityControlView.swift
 //  iOSUnityBridgeDemo
 //
 //  Created by Roberto Teixeira on 09/06/2026.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct UnityControlView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    UnityControlView()
 }
