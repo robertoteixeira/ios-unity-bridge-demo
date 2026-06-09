b359f63930976613a07f8830c371415f71cbb58f
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2026-06-09 18:37:51 -0300

    Create native iOS SwiftUI app

---
+++ ios/iOSUnityBridgeDemo/iOSUnityBridgeDemo/iOSUnityBridgeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSUnityBridgeDemoApp.swift
+//  iOSUnityBridgeDemo
+//
+//  Created by Roberto Teixeira on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct iOSUnityBridgeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
