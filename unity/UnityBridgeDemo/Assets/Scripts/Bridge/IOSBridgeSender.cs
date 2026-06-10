06bf12ff2f8dbdd2ea700529c606863c5198a98d
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2026-06-10 11:35:51 -0300

    Add iOS bridge sender script

---
+++ unity/UnityBridgeDemo/Assets/Scripts/Bridge/IOSBridgeSender.cs
@@ -0,0 +1,23 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class IOSBridgeSender : MonoBehaviour
+{
+#if UNITY_IOS && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    private static extern void SendMessageToIOS(string message);
+#endif
+
+    public void SendMessage(string type, string payload = "{}")
+    {
+        string message = $"{{\"type\":\"{type}\",\"payload\":{payload}}}";
+
+#if UNITY_IOS && !UNITY_EDITOR
+        SendMessageToIOS(message);
+#else
+        Debug.Log($"[IOSBridgeSender] {message}");
+#endif
+
+
+    }
+}
