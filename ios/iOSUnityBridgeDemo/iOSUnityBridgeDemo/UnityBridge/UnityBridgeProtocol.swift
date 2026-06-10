09fa220e7dd29e53d1838d8d65865db6d9fa4079
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2026-06-10 06:43:42 -0300

    Add Unity bridge protocol

--- ios/iOSUnityBridgeDemo/iOSUnityBridgeDemo/UnityBridge/UnityBridgeProtocol.swift
+++ ios/iOSUnityBridgeDemo/iOSUnityBridgeDemo/UnityBridge/UnityBridgeProtocol.swift
@@ -5,3 +5,14 @@
 //  Created by Roberto Teixeira on 10/06/2026.
 //
 
+import Foundation
+
+@MainActor
+protocol UnityBridgeProtocol: ObservableObject {
+    var state: UnityBridgeState { get }
+    var events: [UnityEvent] { get }
+
+    func loadUnity()
+    func unloadUnity()
+    func sendCommand(_ command: UnityCommand)
+}
